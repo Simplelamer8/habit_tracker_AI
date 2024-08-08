4bef7656d2ca47133828ba33e0c535e583e062bb
Simplelamer8 <67641980+Simplelamer8@users.noreply.github.com>
2024-08-08 22:17:48 +0500

    Update db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,13 +1,14 @@
-import {Pool} from 'pg'
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
-const pool = new Pool ({
-    user: "postgres",
-    password: "qwerty",
-    host: "localhost",
-    port: 5432,
-    database: "habit_tracker_ai"
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER_NAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB,
 });
-
-
 
 export default pool;
