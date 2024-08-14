233abcf5f0c3316019e9b2fd80a5b22e2352ede4
Simplelamer8 <67641980+Simplelamer8@users.noreply.github.com>
2024-08-14 19:56:36 +0500

    Update database.py

--- backend_ai/database.py
+++ backend_ai/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 engine = create_async_engine(
-    "postgresql://postgres:qwerty@localhost:5432/vector_db"
+    "postgresql://postgres_user:habiPmmnpAf8mPWrrfacbSvo8MJzX9RB@dpg-cqqf2hogph6c738ak080-a.frankfurt-postgres.render.com/habit_tracker_ai"
 )
 
 new_session = async_sessionmaker(engine, expire_on_commit=False)
