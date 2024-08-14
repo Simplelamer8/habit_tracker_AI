875ebc628eae8972ac49216594a3fa08604cd581
Simplelamer8 <67641980+Simplelamer8@users.noreply.github.com>
2024-08-14 17:39:19 +0500

    Update db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -7,7 +7,7 @@ const pool = new Pool({
   user: process.env.DB_USER_NAME,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined, // Convert the port to a number
   database: process.env.DB,
 });
 
